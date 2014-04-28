373b51cc9a16591587da75ce1bc881049e481a19
Leginar <logansenicar@logans-mbp.gateway.pace.com>
2014-04-27 19:29:01 -0600

    lotsa changes

---
+++ LudumDare/TapRoot/Assets/DirtDissapear.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+
+function OnTriggerEnter (other : Collider) {
+			gameObject.SetActive(false);
+		Destroy(other.gameObject);
+}
